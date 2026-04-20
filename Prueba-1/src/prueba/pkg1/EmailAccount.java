c0f61c712b189927079827b6ad732bd6fea27304
Marco Vásquez <marco_vasquez1@unitec.edu>
2026-04-20 15:47:00 -0600

    Clases correctas

--- Prueba-1/src/prueba/pkg1/EmailAccount.java
+++ Prueba-1/src/prueba/pkg1/EmailAccount.java
@@ -10,5 +10,65 @@ package prueba.pkg1;
  * @author andres
  */
 public class EmailAccount {
-    
+    private String direccion;
+    private String contraseña;
+    private String nombre;
+    private Email[] inbox;
+    public EmailAccount(String correo, String contraseña, String nombre){
+        this.direccion=direccion;
+        this.contraseña=contraseña;
+        this.nombre=nombre;
+        inbox=new Email[100];
+    }
+    public String getDireccion(){
+        return direccion;    
+    }
+    public String getContra(){
+        return contraseña;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public boolean recibir(Email email){
+        for(int control=0;control<inbox.length;control++){
+            if(inbox[control]==null){
+                inbox[control]=email;
+                return true;
+            }
+        }
+        return false;
+    }
+    public void mostrar(){
+        System.out.println("Cuentra: "+direccion+" | Usuario: "+nombre);
+        int total=0, noLeidos=0;
+        for(int control=0;control<inbox.length;control++){
+            total++;
+            String estado;
+            estado=inbox[control].esLeido() ? "LEIDO":"SIN LEER";
+            if(!inbox[control].esLeido()){
+                noLeidos++;
+            }
+            System.out.println(control+" - "+inbox[control].getEmisor()+" - "+inbox[control].getAsunto()+" - "+estado);
+            System.out.println("\nCorreos sin leer: "+noLeidos);
+            System.out.println("Total correos: "+total);
+        }
+        
+    }
+    public void leerCorreo(int posicion){
+        if(posicion>=0 && posicion<inbox.length && inbox[posicion]!=null){
+            inbox[posicion].imprimirCorreo();
+            inbox[posicion].marcarcorreoLeido();
+        }
+        else{
+            System.out.println("Correo inexistente");
+        }
+    }
+    public void limpiarInbox(){
+        for(int control=0;control<inbox.length;control++){
+            if(inbox[control]!=null && inbox[control].esLeido()){
+                inbox[control]=null;
+            }
+        }
+        System.out.println("Los correos leidos han sido elimindados");
+    }
 }
--- Prueba-1/src/prueba/pkg1/JavaLook.java
+++ Prueba-1/src/prueba/pkg1/JavaLook.java
@@ -4,11 +4,53 @@
  * and open the template in the editor.
  */
 package prueba.pkg1;
-
+import java.util.Scanner;
 /**
  *
  * @author andres
  */
 public class JavaLook {
+    static EmailAccount[] cuentas=new EmailAccount[100];
+    static EmailAccount cuentaactiva=null;
+    static Scanner lector=new Scanner(System.in);
+    public static void menuInicial(){
+        System.out.println("\n1. Login");
+        System.out.println("2. Crear cuenta");
+        System.out.println("Seleccione una opcion: ");
+        int opcion=lector.nextInt();
+        switch(opcion){
+            case 1: 
+                login();
+                break;
+            case 2:
+                crearCuenta();
+                break;
+        }
+    }
+    public static void login(){
+        System.out.println("Correo: ");
+        String correo=lector.nextLine();
+        System.out.println("Contraseña: ");
+        String contra=lector.nextLine();
+        for(EmailAccount account:cuentas){
+            if(account!=null && account.getDireccion().equals(correo) && account.getContra().equals(contra)){
+                cuentaactiva=account;
+                System.out.println("Login exitoso");
+                return;
+            }    
+        }
+        System.out.println("Correo o contraseña incorrecta");
+    }
+    
+    public static void main(String[]args){
+        while(true){
+            if(cuentaactiva==null){
+                menuInicial();
+            }
+            else{
+                menuPrincipal();
+            }
+        }
+    }
     
 }
